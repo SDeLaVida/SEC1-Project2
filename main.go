f316dde1d37f6f8e8a4def7185942552cbc1f9ba
Patrick N <paab@itu.dk>
2023-10-24 17:32:57 +0200

    actually working implementation.

--- main.go
+++ main.go
@@ -63,7 +63,7 @@ func main() {
 		}
 	}()
 
-	// Connects to the other patients
+	// Connects to the other patients and the hospital
 	for i := 0; i <= 3; i++ {
 		port := hospitalID + int32(i)
 
@@ -90,55 +90,74 @@ func main() {
 		fmt.Printf("%v", p.clients[port])
 	}
 
-	scanner := bufio.NewScanner(os.Stdin)
-	if ownPort != hospitalID { // The following logic is at the patient side
-		fmt.Print("Enter a number that you want to share with the other patients. This can overflow so be nice :)\nNumber: ") // This can overflow, so be nice
-		for scanner.Scan() {
-			secret, _ := strconv.ParseInt(scanner.Text(), 10, 32)
-			p.ShareChunks(int(secret)) // Break the secret into chunks and send them to the other patients
-			break
-		}
-		// Wait for the other patients to send their chunks
-		current_chunks := -1 // Ignore this: just some logic for the print statement while we wait
-		for current_chunks < len(p.clients)-1 {
-			if current_chunks != len(p.chunks) {
-				current_chunks = len(p.chunks)
-				fmt.Printf("Patient (ID: %v): Waiting for data from other patients (%v out of %v)\n", p.id, len(p.chunks)+1, len(p.clients))
-			}
-			time.Sleep(2 * time.Second)
-		}
-
-		// Now we have all the chunks, so we can sum them and send them to the hospital
-		sumChunks := p.SumChunks()
-		fmt.Printf("Patient (ID: %v): I got all the data from the other patients!\nMy secret is: %v\n", p.id, sumChunks)
-		fmt.Printf("Patient (ID: %v): Sending my secret to the hospital at id: %v\n", p.id, p.HospitalID)
-		hospitalClient := p.clients[p.HospitalID]
-		request := &MPC.Result{Id: p.id, Result: int32(sumChunks)}
-		reply, err := hospitalClient.SendResult(p.ctx, request)
-
-		if err != nil {
-			log.Fatalf("Patient (ID: %v): Could not send chunk to id:%v : %v", p.id, p.HospitalID, err)
-		}
-		fmt.Printf("Patient (ID: %v): I got reply from id %v: %v\n", p.id, p.HospitalID, reply)
-		return
-	} else { // The following logic is aimed at the hospital side
-
-		for (len(p.chunks)) < len(p.clients) {
-			// Wait for all the patients to send their chunks. Using a channel to avoid race conditions
-			fmt.Printf("Hospital (ID: %v): Waiting for data from other patients. Got %v out of %v\n", p.id, len(p.chunks), len(p.clients))
-			time.Sleep(2 * time.Second)
-			result := <-p.channel
-			fmt.Printf("Hospital (ID: %v): I just got result %v from id %v\n", p.id, result.Result, result.Id)
-			p.chunks[result.Id] = int(result.Result)
-		}
-		for id, chunk := range p.chunks {
-			fmt.Printf("Hospital (ID: %v): Patient (Id: %v) sent the following result: %v\n", p.id, id, chunk)
-		}
-		fmt.Print("Press any button to quit\n")
-		scanner.Scan()
-
+	// Setup is now done and our secure aggregation with TLS implementation begins
+	fmt.Print("\n")
+	if ownPort != hospitalID { // The following logic is only applicable for the patients
+		patientLogic(p)
+	} else { // The following logic is only applicable for the hospital
+		HospitalLogic(p)
 	}
 
+}
+
+func patientLogic(p *peer) {
+	// The following logic is at the patient side
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("Patient (ID: %v): Enter a number that you want to share with the other patients. This can overflow so be nice :)\n", p.id) // This can overflow, so be nice
+	for scanner.Scan() {
+		secret, _ := strconv.ParseInt(scanner.Text(), 10, 32)
+		fmt.Printf("Patient (ID: %v): my secret number is %v. Performing secure aggregation\n", p.id, secret)
+
+		p.ShareChunks(int(secret)) // Break the secret into chunks and send them to the other patients
+		break
+	}
+
+	// Wait for the other patients to send their chunks
+	current_chunks := -1 // Ignore this: just some logic for the print statement while we wait
+	for current_chunks < len(p.clients) {
+		if current_chunks != len(p.chunks) {
+			current_chunks = len(p.chunks)
+			fmt.Printf("Patient (ID: %v): Waiting for data from other patients (%v out of %v)\n", p.id, len(p.chunks)-1, len(p.clients)-1)
+		}
+		time.Sleep(2 * time.Second)
+	}
+
+	// Now we have all the chunks, so we can sum them and send them to the hospital
+	sumChunks := p.SumChunks()
+	fmt.Printf("Patient (ID: %v): I got all the data from the other patients!\n", p.id)
+	fmt.Printf("Patient (ID: %v): My aggregated value is: %v\n", p.id, sumChunks)
+	fmt.Printf("Patient (ID: %v): Sending my secret to the hospital at id: %v\n", p.id, p.HospitalID)
+	hospitalClient := p.clients[p.HospitalID]
+	request := &MPC.Result{Id: p.id, Result: int32(sumChunks)}
+	reply, err := hospitalClient.SendResult(p.ctx, request)
+
+	if err != nil {
+		log.Fatalf("Patient (ID: %v): Could not send chunk to id:%v : %v", p.id, p.HospitalID, err)
+	}
+	fmt.Printf("Patient (ID: %v): I got reply from the hospital (id: %v):\n %v\n", p.id, p.HospitalID, reply)
+	time.Sleep(6 * time.Second)
+}
+
+func HospitalLogic(p *peer) {
+	// The following logic is aimed at the hospital side
+	scanner := bufio.NewScanner(os.Stdin)
+	for (len(p.chunks)) < len(p.clients) {
+		// Wait for all the patients to send their chunks. Using a channel to avoid race conditions
+		fmt.Printf("Hospital (ID: %v): Waiting for data from other patients. Got %v out of %v\n", p.id, len(p.chunks), len(p.clients))
+		time.Sleep(2 * time.Second)
+		result := <-p.channel
+		fmt.Printf("Hospital (ID: %v): I just got result %v from id %v\n", p.id, result.Result, result.Id)
+		p.chunks[result.Id] = int(result.Result)
+	}
+	superChunk := int(0)
+	for id, chunk := range p.chunks {
+		fmt.Printf("Hospital (ID: %v): Patient (Id: %v) sent the following result: %v\n", p.id, id, chunk)
+		superChunk += chunk
+	}
+	fmt.Printf("Hospital (ID: %v): The total sum for all the patients is %v\n", p.id, superChunk)
+
+	fmt.Print("Press any button to quit\n")
+	scanner.Scan()
 }
 
 func (p *peer) SendChunk(ctx context.Context, req *MPC.Request) (*MPC.Reply, error) {
@@ -153,20 +172,24 @@ func (p *peer) SendResult(ctx context.Context, req *MPC.Result) (*MPC.Reply, err
 	// Receive result from other peers and store them in a the chunks array.
 	// Should in a perfect world only be called for the hospital connection.
 	p.channel <- Result{Id: req.Id, Result: req.Result}
-	rString := fmt.Sprintf("Hopsital (ID: %v): I got %v from Patient (ID: %v)", p.id, req.Result, req.Id)
+	rString := fmt.Sprintf("Hospital (ID: %v): I got %v from Patient (ID: %v)", p.id, req.Result, req.Id)
 	return &MPC.Reply{Reply: rString}, nil
 }
 
-func splitChunks(secret int, n int) []int {
+func splitChunks(secret int, fromId int, n int) map[int32]int {
 	// Split the secret into chunks, while avoiding floating point numbers but still keeping the sum of the chunks equal to the secret
-	temp_secret := secret
-	chunks := []int{}
-	for i := 0; i < n-1; i++ { // n-1 because we want to keep the last chunk as the remainder (might leak information? Unsure how to avoid this)
-		rand := rand.Intn(temp_secret)
-		chunks = append(chunks, rand)
-		temp_secret -= rand
+	remainder := secret
+	chunks := make(map[int32]int)
+	for i := 0; i < n-1; i++ { // n-1 because we want to keep the last chunk as the remainder (might leak information about the previous chunk, but not really? Unsure how you would avoid this)
+		rand := rand.Intn(remainder)
+		// Just go things, as the random generated number will always be between 0 and the parsed value.
+		// Meaning there is a case where the rand is == 0 and this should be accounted for
+		chunks[int32((fromId + i))] = rand
+		fmt.Printf("Iteration %v (for id:%v): Generated following chunk: %v from the remainder: %v and our secret was: %v\n", i, fromId+i, rand, remainder, secret)
+		remainder -= rand
 	}
-	chunks = append(chunks, temp_secret)
+	fmt.Printf("Iteration %v (for id:%v): Generated following chunk: %v from the remainder: %v and our secret was: %v\n", n-1, fromId+n-1, remainder, remainder, secret)
+	chunks[int32((fromId + n - 1))] = remainder // The remainder gets parsed to the last peer
 	return chunks
 
 }
@@ -180,26 +203,35 @@ func (p *peer) SumChunks() int {
 	return sum
 }
 
+func debugSumChunks(chunko []int) int {
+	sum := int(0)
+	for _, chunk := range chunko {
+		sum += chunk
+	}
+	return sum
+}
+
 func (p *peer) ShareChunks(secret int) {
 	// Split the secret into chunks
-	patient_amount := len(p.clients) - 1 // minus one as the hospital is not a patient but is still in the clients map
-	split := splitChunks(secret, patient_amount)
+	patient_amount := len(p.clients) // We don't want a chunk for the hospital, but as our peer is not in the client list, it evens out.
+	split := splitChunks(secret, int(p.HospitalID)+1, patient_amount)
+
+	fmt.Printf("Patient (ID: %v): i take my share (chunk: %v) of the chunks\n", p.id, split[p.id])
+	p.chunks[p.id] = split[p.id] // I take my share
+
 	for id, client := range p.clients {
 		if id == p.HospitalID {
 			fmt.Printf("Patient (ID: %v): I skip sending a chunk to the hospital at id: %v\n", p.id, id)
 			continue
 		}
-		split_id := int(id) % patient_amount // Our port number is 5000 + id, so we need to mod it with the number of chunks. A consequence of the implementation (splitChunks function)
-		if id == p.id {
-			fmt.Printf("Patient (ID: %v): I keep chunk %v\n", p.id, split[split_id])
-			p.chunks[id] = split[split_id]
-		}
-		request := &MPC.Request{Chunk: int32(split[split_id]), Id: p.id}
+
+		request := &MPC.Request{Chunk: int32(split[id]), Id: p.id}
 		reply, err := client.SendChunk(p.ctx, request)
 		if err != nil {
 			log.Fatalf("Patient (ID: %v): Could not send chunk to patient (id:%v): %v", p.id, id, err)
 		}
-		fmt.Printf("Patient (ID: %v): I got reply from patient (id %v): %v\n", p.id, id, reply)
+		fmt.Printf("Patient (ID: %v): I got reply from patient (id %v):\n %v\n", p.id, id, reply)
+
 	}
 }
 
